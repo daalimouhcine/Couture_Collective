787bbd81b0be1cbe8c41fa2119f4a5ef691f5477
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-03-22 00:17:37 +0000

    add architecture for the angular project

--- Project/front-end/src/app/app.module.ts
+++ Project/front-end/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
