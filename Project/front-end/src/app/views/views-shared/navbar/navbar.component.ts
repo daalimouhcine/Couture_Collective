1bf966123356f716af2d60ccaebe6e7347bed0de
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-04-12 10:24:43 +0000

    add logout

--- Project/front-end/src/app/views/views-shared/navbar/navbar.component.ts
+++ Project/front-end/src/app/views/views-shared/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { CurrentUser } from 'src/app/features/users/interfaces/users';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user: CurrentUser = new CurrentUser();
+  profileOpen: boolean = false;
 
+  constructor(private authService: AuthService) {};
+  ngOnInit(): void {
+    this.user = this.authService.getUserFromLocalStorage();
+  }
+
+  logout() {
+    this.authService.logoutWithGuard();
+  }
 }
